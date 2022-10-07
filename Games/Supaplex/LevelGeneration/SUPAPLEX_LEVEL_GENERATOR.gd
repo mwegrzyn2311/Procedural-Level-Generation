894ac4b1636dbcaf605cca1f49f0e1a4b899f5f2
Michal23 <michal.wegrzyn2137@gmail.com>
2022-10-07 16:07:36 +0200

    tmp

--- Games/Supaplex/LevelGeneration/SUPAPLEX_LEVEL_GENERATOR.gd
+++ Games/Supaplex/LevelGeneration/SUPAPLEX_LEVEL_GENERATOR.gd
@@ -1,13 +1,34 @@
 extends Node
 
-var Player = preload("res://Games/Supaplex/Elements/Player/player.tscn")
-
 func generate_level(width: int, height: int, difficulty: int) -> Array:
 	# Hardcoded array for testing purposes
 	return [
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.GRASS, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.GRASS, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.GRASS, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.BOULDER, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.POINT, Vector2(1,1)),
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.POINT, Vector2(1,1)),
 #		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.PLAYER, Vector2(1,1)),
-#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(5,5)),
-#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(7,5)),
-#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(5,6)),
-#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.WALL, Vector2(7,6)),
-	]
+#		TILEMAP_UTILS.ele_instance(TILE_ELEMENTS.EXIT, Vector2(1,1)),
+]
