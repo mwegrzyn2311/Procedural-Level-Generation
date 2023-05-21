52db1712b66ac89e271d1a88d71d0c2bfe549007
Michal23 <michal.wegrzyn2137@gmail.com>
2023-05-21 21:10:00 +0200

    Optimize MCTS a little bit

--- Games/Supaplex/LevelGeneration/MonteCarloGeneration/mcts_supaplex_state.gd
+++ Games/Supaplex/LevelGeneration/MonteCarloGeneration/mcts_supaplex_state.gd
@@ -2,17 +2,20 @@ extends MCTSGameState
 
 class_name MCTSSupaplexState
 
-var res: Dictionary = {}
+var res: Dictionary
 var walls_to_delete: int
 var points_to_place: int
 var initial_player_pos: Vector2
 var player_pos: Vector2
-var walls_deleted: int = 0
-var rocks_placed: int = 0
-var points_placed: int = 0
+var walls_deleted: int
+var rocks_placed: int
+var points_placed: int
 var last_dir: Vector2
 
 var curr_legal_actions: Array
+var curr_legal_move_actions: Array
+
+var gen_res: float
 
 func _init(res: Dictionary, walls_to_delete: int, points_to_place: int, initial_player_pos: Vector2, player_pos: Vector2, walls_deleted: int = 0, rocks_placed: int = 0, points_placed: int = 0, last_dir: Vector2 = Vector2(0, 0)):
 	self.res = res
@@ -24,7 +27,8 @@ func _init(res: Dictionary, walls_to_delete: int, points_to_place: int, initial_
 	self.rocks_placed = rocks_placed
 	self.points_placed = points_placed
 	self.last_dir = last_dir
-	self.curr_legal_actions = self.generate_legal_actions()
+	self.generate_legal_actions()
+	self.calc_gen_res()
 	
 static func new_initial_state(width: int, height: int) -> MCTSSupaplexState:
 	var res: Dictionary = {}
@@ -37,8 +41,8 @@ static func new_initial_state(width: int, height: int) -> MCTSSupaplexState:
 	var points_to_place = int(RNG_UTIL.RNG.randf_range(0.15, 0.2) * walls_to_delete)
 	return MCTSSupaplexState.new(res, walls_to_delete, points_to_place, initial_player_pos, initial_player_pos)
 
-func copy(res: Dictionary, walls_deleted: int, player_pos: Vector2, rocks_placed: int, points_placed: int, last_dir: Vector2) -> MCTSSupaplexState:
-	return MCTSSupaplexState.new(res, walls_deleted, points_to_place, initial_player_pos, player_pos, walls_deleted, rocks_placed, points_placed, last_dir)
+func copy(res: Dictionary, new_player_pos: Vector2, new_walls_deleted: int, new_rocks_placed: int, new_points_placed: int, new_last_dir: Vector2) -> MCTSSupaplexState:
+	return MCTSSupaplexState.new(res, walls_to_delete, points_to_place, initial_player_pos, new_player_pos, new_walls_deleted, new_rocks_placed, new_points_placed, new_last_dir)
 
 const NIEGH_CORRID_MULT: float = 1.0
 const NIEGH_DIAG_NON_CORRID_MULT: float = 5.0
@@ -60,18 +64,17 @@ func max_score_at_pos() -> float:
 	return NIEGH_CORRID_MULT * 4.0 + NIEGH_DIAG_NON_CORRID_MULT * 4.0
 
 func move_player(args: Dictionary) -> MCTSSupaplexState:
-	var new_res = self.res.duplicate(true)
-	var dir = args["dir"]
+	var new_res: Dictionary = self.res.duplicate(true)
+	var dir: Vector2 = args["dir"]
 	var dest: Vector2 = self.player_pos + dir
 	assert(self.res.has(dest))
-	if new_res[dest] == TILE_ELEMENTS.Ele.WALL:
-		self.walls_deleted += 1
+	var new_walls_deleted = walls_deleted + 1 if new_res[dest] == TILE_ELEMENTS.Ele.WALL else walls_deleted
 	var ele_to_place = element_to_place()
 	new_res[self.player_pos] = ele_to_place
-	var new_points_placed: int = points_placed + 1 if ele_to_place == TILE_ELEMENTS.Ele.POINT else ele_to_place
+	var new_points_placed: int = points_placed + 1 if ele_to_place == TILE_ELEMENTS.Ele.POINT else points_placed
 	new_res[dest] = TILE_ELEMENTS.Ele.PLAYER
-	return self.copy(new_res, walls_deleted, dest, rocks_placed, new_points_placed, dir)
-	
+	return self.copy(new_res, dest, new_walls_deleted, rocks_placed, new_points_placed, dir)
+
 func element_to_place() -> TILE_ELEMENTS.Ele:
 	var point_chance = float(points_to_place - points_placed) / float(walls_to_delete - walls_deleted)
 	if RNG_UTIL.RNG.randf() <= point_chance:
@@ -88,20 +91,29 @@ func place_boulder(args: Dictionary) -> MCTSSupaplexState:
 #	if TILE_ELEMENTS.is_corridor(dest_ele):
 #		self.walls_deleted -= 1
 	new_res[args["pos"]] = TILE_ELEMENTS.Ele.BOULDER
-	return self.copy(new_res, new_walls_deleted, Vector2(player_pos), rocks_placed + 1, points_placed, Vector2(last_dir))
+	return self.copy(new_res, Vector2(player_pos), new_walls_deleted, rocks_placed + 1, points_placed, Vector2(last_dir))
 
 func finish_generation():
 	res[initial_player_pos] = TILE_ELEMENTS.Ele.PLAYER
 	res[player_pos] = TILE_ELEMENTS.Ele.EXIT
 
-func generate_legal_actions() -> Array:
-	return legal_move_actions() + legal_boulder_actions()
+func generate_legal_actions():
+	self.curr_legal_move_actions = legal_move_actions()
+	self.curr_legal_actions = self.curr_legal_move_actions + legal_boulder_actions()
+
+func calc_gen_res():
+	if walls_to_delete != walls_deleted:
+		return -1.0
+	var total = 0.0
+	for pos in res:
+		total += score_at_pos(pos)
+	self.gen_res = total / (max_score_at_pos() * self.walls_to_delete)
 
 # =======================
 # Abstract functions overrides here
-	
+
 func legal_actions() -> Array:
-	return self.curr_legal_actions
+	return self.curr_legal_actions.duplicate()
 
 func legal_move_actions() -> Array[MCTSAction]:
 	var legal_move_actions: Array[MCTSAction] = []
@@ -109,7 +121,7 @@ func legal_move_actions() -> Array[MCTSAction]:
 		if unit_vector != -last_dir and self.res.has(self.player_pos + unit_vector) and not (self.res.has(self.player_pos + unit_vector + Vector2.UP) and self.res[self.player_pos + unit_vector + Vector2.UP] == TILE_ELEMENTS.Ele.BOULDER):
 			legal_move_actions.append(MCTSAction.new(Callable(self, "move_player"), {"dir": unit_vector}))
 	return legal_move_actions
-	
+
 func legal_boulder_actions() -> Array[MCTSAction]:
 	var boulder_potential_pos: Vector2 = self.player_pos + Vector2.UP
 	if res.has(boulder_potential_pos) and not TILE_ELEMENTS.is_corridor(res[boulder_potential_pos]):
@@ -118,18 +130,13 @@ func legal_boulder_actions() -> Array[MCTSAction]:
 		return []
 
 func generation_result() -> float:
-	if walls_to_delete != walls_deleted:
-		return -1.0
-	var total = 0.0
-	for pos in res:
-		total += score_at_pos(pos)
-	return total / (max_score_at_pos() * self.walls_to_delete)
+	return self.gen_res
 	
 func is_generation_completed() -> bool:
-	if walls_to_delete == walls_deleted || legal_move_actions().is_empty():
+	if walls_to_delete == walls_deleted || curr_legal_move_actions.is_empty():
 		finish_generation()
 		return true
 	return false
 
-func get_res_dict() -> Dictionary:
+func get_level_dict() -> Dictionary:
 	return self.res
